ed4f4e0802aa86365e04f01184fca9f8e35531da
Scream <scream870102@gmail.com>
2021-03-01 01:54:52 +0900

    Add utils for mipmap tile generator

---
+++ Assets/Scripts/Tilemap/TilemapGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+namespace Scream.SonaruIsugeTank
+{
+    [ExecuteAlways]
+    class TilemapGenerator : MonoBehaviour
+    {
+        public AlternativeTilemap[] AlternativeTilemaps = default;
+        [ExecuteAlways]
+        public Tilemap TilemapToChange = null;
+
+    }
+
+
+
+    [System.Serializable]
+    struct AlternativeTilemap
+    {
+        public TileBase From;
+        public TileBase To;
+    }
+}
+
