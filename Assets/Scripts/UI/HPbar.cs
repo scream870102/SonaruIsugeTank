a34e3b3c4d8d2f51a085421b171818908a68b042
SonaruIsuge <bread511206@gmail.com>
2021-03-09 16:14:47 +0800

    tmp

--- Assets/Scripts/UI/HPbar.cs
+++ Assets/Scripts/UI/HPbar.cs
@@ -6,36 +6,33 @@ using UnityEngine.UI;
 public class HPbar : MonoBehaviour
 {
     public Image HpBarImg;
-    public GameObject player;
+    [SerializeField] Player player;
     private float playerHealth;
     private float HPpercent;
 
-    void OnEnable() 
+    void OnEnable()
     {
         Player.PlayerHpChange += UpdateHpBar;
     }
 
-    void OnDisable() 
+    void OnDisable()
     {
-        Player.PlayerHpChange -= UpdateHpBar;    
+        Player.PlayerHpChange -= UpdateHpBar;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        playerHealth = player.GetComponent<Player>().property.health;
+        playerHealth = player.property.health;
         HPpercent = 1.0f;
-        HpBarImg.color = Color.Lerp (Color.red, Color.green, HPpercent);
-        
+        HpBarImg.color = Color.Lerp(Color.red, Color.green, HPpercent);
+
     }
 
     public void UpdateHpBar(int currentHealth)
     {
-        if(player != null)
-        {
-            HPpercent = currentHealth / playerHealth;
-            HpBarImg.fillAmount = Mathf.Lerp(0.25f, 1, HPpercent);
-            HpBarImg.color = Color.Lerp(Color.red, Color.green, HPpercent);
-        }
+        HPpercent = currentHealth / playerHealth;
+        HpBarImg.fillAmount = Mathf.Lerp(0.25f, 1, HPpercent);
+        HpBarImg.color = Color.Lerp(Color.red, Color.green, HPpercent);
     }
 }
